0772cf4cc795cc507814489f0e2ca3c805d6e357
kissgabi <szak.dolgozat@sryer.com>
2020-02-21 14:27:59 +0100

    update Kiss József Gábor - Szakdolgozat 2020.odt

---
+++ V00-01kkvDBdrop_database.sql
@@ -0,0 +1,16 @@
+/* START OF FILE -----------------------------------------------------------------*/
+
+
+/* CREATE DATABASE ---------------------------------------------------------------*/
+
+/* KKV SAMPLE DATABASE 2019-2020         */
+/*                   X                   */
+/* DROP DATABASE IF EXISTS `kkv_sample`; */
+/*                   X                   */
+/* CREATE DB, IF NOT EXISTS              */
+
+DROP DATABASE IF EXISTS `kkv_sample`;
+
+
+/* END OF FILE -------------------------------------------------------------------*/
+
